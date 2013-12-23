880d4b2a4e3d9e7b6a2aa827d0615d57399c7bf2
Jonathan Kim <jkimbo@gmail.com>
2013-12-23 17:03:10 +0000

    Added singleton App class

---
+++ src/FelixOnline/Core/App.php
@@ -0,0 +1,123 @@
+<?php
+/**
+ * App class
+ */
+class App
+{
+	protected static $instance = null;
+	protected static $options = array();
+	public static $db = null;
+	protected static $safesql = null;
+
+	/**
+	 * Constructor
+	 *
+	 * @param array $options - options array
+	 */
+	public function __construct($options = array())
+	{
+		global $db;
+		global $safesql;
+		$this->checkOptions($options);
+		$this->options = $options;
+
+		// Initialise ezSQL database connection
+		if ($db) {
+			self::$db = $db;
+		} else {
+			self::$db = new ezSQL_mysqli();
+			self::$db->quick_connect(
+				$this->getOption('db_user'),
+				$this->getOption('db_pass'),
+				$this->getOption('db_name'),
+				$this->getOption('db_host', 'localhost'),
+				'utf8'
+			);
+		}
+
+		if ($safesql) {
+			self::$safesql = $safesql;
+		} else {
+			self::$safesql = new SafeSQL_MySQLi(self::$db->dbh);
+		}
+
+		self::$instance = $this;
+	}
+
+	/**
+	 * Check that all required options are defined
+	 *
+	 * Throws InternalException if option is not defined
+	 */
+	private function checkOptions($options)
+	{
+		$required = array(
+			'db_name',
+			'db_user',
+			'db_pass'
+		);
+
+		foreach($required as $req) {
+			if (!array_key_exists($req, $options)) {
+				throw new InternalException('"' . $req . '" option is not defined');
+			}
+		}
+	}
+
+	/**
+	 * Get instance
+	 *
+	 * @return instance
+	 */
+	public static function getInstance()
+	{
+		if (is_null(self::$instance)) {
+			self::$instance = new App();
+		}
+		return self::$instance;
+	}
+
+	/**
+	 * Set instance
+	 *
+	 * @param object $instance - instance object to set
+	 *
+	 * @return void
+	 */
+	public static function setInstance($instance)
+	{
+		self::$instance = $instance;
+	}
+
+	/**
+	 * Get option
+	 *
+	 * @param string $key - option key
+	 * @param mixed $default - value to return if not defined [optional]
+	 *
+	 * @return mixed option
+	 */
+	public function getOption($key)
+	{
+		if (!array_key_exists($key, $this->options)) {
+			// if a default has been defined 
+			if (func_get_arg(1)) {
+				return func_get_arg(1);
+			} else {
+				throw new InternalException('Key "'.$key.'" has not been set');
+			}
+		}
+		return $this->options[$key];
+	}
+
+	/**
+	 * Get safesql query
+	 *
+	 * @param string $sql - sql string
+	 * $param array $values - array of values for query
+	 */
+	public static function query($sql, $values)
+	{
+		return self::$safesql->query($sql, $values);
+	}
+}
