b37e401267bf93677044379b4b0974ed3883a724
Jonathan Kim <jkimbo@gmail.com>
2014-01-22 14:21:15 +0000

    Refactored BaseManager and added tests

--- src/FelixOnline/Core/BaseManager.php
+++ src/FelixOnline/Core/BaseManager.php
@@ -1,19 +1,250 @@
 <?php
 namespace FelixOnline\Core;
+
+use \FelixOnline\Exceptions\InternalException;
+
 /**
  * Base manager
  */
 class BaseManager
 {
-	protected $table; // database table
-	protected $class; // object class name
+	/**
+	 * database table
+	 */
+	protected $table;
+
+	/**
+	 * object class name
+	 */
+	protected $class;
+
+	/**
+	 * primary key
+	 */
+	protected $pk = 'id';
+
+	/**
+	 * Array of query filters
+	 */
+	protected $filters = array();
+
+	/**
+	 * Order statement
+	 */
+	protected $order;
+
+	/**
+	 * Limit
+	 */
+	protected $limit;
+
+	/**
+	 * Get all objects
+	 */
+	public function all()
+	{
+		$_filters = $this->filters; // store filters
+		$this->filters = array(); // reset them
+
+		$values = $this->values();
+
+		$this->filters = $_filters; // restore filters
+
+		return $values;
+	}
+
+	/**
+	 * Filter objects
+	 */
+	public function filter($filter)
+	{
+		$this->filters[] = $filter;
+		return $this;
+	}
+
+	/**
+	 * Order objects
+	 */
+	public function order($columns, $order)
+	{
+		$this->order = array($columns, $order);
+		return $this;
+	}
+
+	/**
+	 * Add limit to query
+	 */
+	public function limit($offset, $number)
+	{
+		$this->limit = array($offset, $number);
+		return $this;
+	}
+
+	/**
+	 * Get count
+	 */
+	public function count()
+	{
+		$statement = [];
+
+		$statement[] = "SELECT COUNT(`" . $this->pk . "`) AS count";
+		$statement[] = $this->getFrom();
+		$statement[] = $this->getWhere();
+		$statement[] = $this->getOrder();
+		$statement[] = $this->getLimit();
+
+		// Remove null values
+		$statement = array_filter($statement);
+
+		$sql = $this->safe(implode(" ", $statement));
+
+		$results = $this->query($sql);
+
+		return (int) $results[0]->count;
+	}
+
+	/**
+	 * Get values
+	 */
+	public function values()
+	{
+		$sql = $this->getSQL();
+
+		$results = $this->query($sql);
+
+		$models = $this->resultToModels($results);
+
+		return $models;
+	}
+
+	/**
+	 * Get sql
+	 */
+	public function getSQL()
+	{
+		$statement = [];
+
+		$statement[] = "SELECT `" . $this->pk . "`";
+		$statement[] = $this->getFrom();
+		$statement[] = $this->getWhere();
+		$statement[] = $this->getOrder();
+		$statement[] = $this->getLimit();
+
+		// Remove null values
+		$statement = array_filter($statement);
+
+		return $this->safe(implode(" ", $statement));
+	}
+
+	/**
+	 * From
+	 */
+	protected function getFrom()
+	{
+		return "FROM `" . $this->table . "`";
+	}
+
+	/**
+	 * Where
+	 */
+	protected function getWhere()
+	{
+		if (!empty($this->filters)) {
+			return "WHERE " . implode(" AND ", $this->filters);
+		}
+		return null;
+	}
+
+	/**
+	 * Order
+	 */
+	protected function getOrder()
+	{
+		if ($this->order) {
+			$order = "ORDER BY ";
+
+			if (is_array($this->order[0])) {
+				$columns = array();
+				foreach ($this->order[0] as $column) {
+					$columns[] = "`" . $column . "`";
+				}
+				$order .= implode(",", $columns);
+			} else {
+				$order .= "`" . $this->order[0] . "`";
+			}
+
+			$order .= " ";
+
+			$order .= $this->order[1];
+
+			return $order;
+		}
+		return null;
+	}
+
+	/**
+	 * Limit
+	 */
+	protected function getLimit()
+	{
+		if ($this->limit) {
+			return "LIMIT " . implode(", ", $this->limit);
+		}
+		return null;
+	}
+
+	/**
+	 * Safe
+	 */
+	protected function safe($sql)
+	{
+		$app = \FelixOnline\Core\App::getInstance();
+
+		return $app['safesql']->query($sql, array());
+	}
+
+	/**
+	 * Query sql
+	 */
+	protected function query($sql)
+	{
+		$app = \FelixOnline\Core\App::getInstance();
+
+		// TODO cache here
+
+		$results = $app['db']->get_results($sql);
+
+		if ($app['db']->last_error) {
+			throw new InternalException($app['db']->last_error);
+		}
+
+		if (is_null($results)) {
+			throw new InternalException('DB query returned no values');
+		}
+
+		return $results;
+	}
+
+	/**
+	 * Map result to models
+	 */
+	protected function resultToModels($result)
+	{
+		$models = array();
+		foreach ($result as $r) {
+			$pk = $r->{$this->pk};
+			$models[] = new $this->class($pk);
+		}
+		return $models;
+	}
 
 	/**
 	 * Get object based on id
 	 *
 	 * $id - array or single id
 	 */
-	public function get($id) {
+	public function get($id)
+	{
 		if (is_array($id)) {
 			$objects = [];
 			foreach($id as $i) {
@@ -26,57 +257,4 @@ class BaseManager
 		}
 	}
 
-	/**
-	 * Get all objects
-	 */
-	public function all($limit = NULL, $order = array("id", "ASC")) {
-		return $this->filter(NULL, $limit, $order);
-	}
-
-	/**
-	 * Filter objects
-	 *
-	 * Example:
-	 *
-	 *	$articleManager->filter(array(
-	 *		'published IS NOT NULL',
-	 *		'published < NOW()'
-	 *	), 20, array('published', 'ASC'));
-	 */
-	public function filter($filters = NULL, $limit = NULL, $order = array("id", "ASC")) {
-		$sql = "SELECT
-					`id`
-				FROM %s";
-
-		$params = array(
-			$this->table
-		);
-
-		if (!is_null($filters)) {
-			$filter = implode(" AND ", $filters);
-
-			$sql .= " WHERE %s";
-			$params[] = $filter;
-		}
-
-		$sql .= " ORDER BY %s %s";
-		$params[] = $order[0];
-		$params[] = $order[1];
-
-		if (!is_null($limit)) {
-			$sql .= " LIMIT 0, %i";
-			$params[] = $limit;
-		}
-
-		$sql = App::query($sql, $params);
-
-		$results = App::$db->get_results($sql);
-		$objects = [];
-
-		foreach($results as $result) {
-			$objects[] = new $this->class($result->id);
-		}
-
-		return $objects;
-	}
 }
---
+++ tests/FelixOnline/Core/BaseManagerTest.php
@@ -0,0 +1,106 @@
+<?php
+
+require_once __DIR__ . '/../../AppTestCase.php';
+require_once __DIR__ . '/../../utilities.php';
+
+class BaseManagerTest extends AppTestCase
+{
+	public $fixtures = array(
+		'articles',
+	);
+
+	public function getManager()
+	{
+		$manager = $this->mock('FelixOnline\\Core\\BaseManager')
+			->new();
+
+		$manager->this()->table = 'article';
+		$manager->this()->class = 'FelixOnline\\Core\\Article';
+
+		return $manager;
+	}
+
+	public function testSQL()
+	{
+		$manager = $this->getManager();
+
+		$manager->filter("title IS NOT NULL")
+			->filter("category IS NOT NULL")
+			->order('id', 'DESC')
+			->limit(0, 10);
+
+		$sql = $manager->getSQL();
+
+		$this->assertEquals($sql, 'SELECT `id` FROM `article` WHERE title IS NOT NULL AND category IS NOT NULL ORDER BY `id` DESC LIMIT 0, 10');
+	}
+
+	public function testAll()
+	{
+		$manager = $this->getManager();
+
+		$all = $manager->all();
+
+		$this->assertCount(3, $all);
+		$this->assertInstanceOf('FelixOnline\Core\Article', $all[0]);
+	}
+
+	public function testFilter()
+	{
+		$manager = $this->getManager();
+
+		$filtered = $manager->filter('published IS NOT NULL')
+			->filter('`id` IN (1, 2)')
+			->values();
+		// OR
+		//$filtered = $manager->filter(array('published IS NOT NULL', 'published < NOW()'));
+
+		$this->assertCount(2, $filtered);
+		$this->assertInstanceOf('FelixOnline\Core\Article', $filtered[0]);
+	}
+
+	public function testOrder()
+	{
+		$manager = $this->getManager();
+
+		$query = $manager->filter('published IS NOT NULL')
+			->filter('`id` IN (1, 2)')
+			->order('id', 'ASC');
+
+		// OR
+		//$ordered = $filtered->order(array('id', 'timestamp'), 'DESC');
+
+		$results = $query->values();
+
+		$this->assertCount(2, $results);
+		$this->assertInstanceOf('FelixOnline\Core\Article', $results[0]);
+		$this->assertEquals($results[0]->getId(), 1);
+	}
+
+	public function testLimit()
+	{
+		$manager = $this->getManager();
+
+		$query = $manager->filter('published IS NOT NULL')
+			->filter('published < NOW()')
+			->order('id', 'ASC');
+
+		$query->limit(0, 1);
+
+		$results = $query->values();
+
+		$this->assertCount(1, $results);
+	}
+
+	public function testCount()
+	{
+		$manager = $this->getManager();
+
+		$query = $manager->filter('published IS NOT NULL')
+			->filter('`id` IN (1, 2)')
+			->order('id', 'ASC');
+
+		$count = $query->count();
+
+		$this->assertEquals($count, 2);
+	}
+}
