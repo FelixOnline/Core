cd1b5401d1e9b73f4a239c9f7b62ce6f5f5e7936
Jonathan Kim <jkimbo@gmail.com>
2014-01-28 11:45:33 +0000

    Added comment manager

---
+++ src/FelixOnline/Core/CommentManager.php
@@ -0,0 +1,10 @@
+<?php
+namespace FelixOnline\Core;
+/**
+ * Comment manager
+ */
+class CommentManager extends BaseManager
+{
+	public $table = 'comment';
+	public $class = 'FelixOnline\Core\Comment';
+}
