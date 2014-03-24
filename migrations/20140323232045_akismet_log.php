5e1dd61a08acd403e894b3b91b689983546e8e4d
Philip Kent <philip.kent@me.com>
2014-03-24 22:04:43 +0000

    Fix migration to have foreign key

--- migrations/20140323232045_akismet_log.php
+++ migrations/20140323232045_akismet_log.php
@@ -23,17 +23,22 @@ class AkismetLog extends AbstractMigration
     public function up()
     {
         $this->execute('
-            CREATE TABLE `akismet_log` (
-              `id` int(11) NOT NULL AUTO_INCREMENT,
-              `comment_id` int(11) NOT NULL,
-              `timestamp` int(11) NOT NULL,
-              `action` varchar(10) NOT NULL,
-              `is_spam` tinyint(1) NOT NULL,
-              `error` text NOT NULL,
-              `request` text NOT NULL,
-              `response` text NOT NULL,
-              PRIMARY KEY (`id`)
-            ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+          CREATE TABLE IF NOT EXISTS `akismet_log` (
+            `id` int(11) NOT NULL AUTO_INCREMENT,
+            `comment_id` int(11) NOT NULL,
+            `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            `action` varchar(10) NOT NULL,
+            `is_spam` tinyint(1) NOT NULL,
+            `error` text NOT NULL,
+            `request` text NOT NULL,
+            `response` text NOT NULL,
+            PRIMARY KEY (`id`),
+            KEY `comment_id` (`comment_id`)
+          ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+
+          ALTER TABLE `akismet_log`
+            ADD CONSTRAINT `comment_link` FOREIGN KEY (`comment_id`) REFERENCES `comment` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
         ');
     }
 
