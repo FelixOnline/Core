1c5f914ae8a2fc6ee3d51dc8e8572a72fc7e5e77
Jonathan Kim <jkimbo@gmail.com>
2014-04-30 12:04:02 +0100

    Updated get most popular comments query

--- src/FelixOnline/Core/ArticleManager.php
+++ src/FelixOnline/Core/ArticleManager.php
@@ -60,27 +60,16 @@ class ArticleManager extends BaseManager
 					article AS id,
 					SUM(count) AS count
 				FROM (
-						(SELECT c.article,COUNT(*) AS count
-						FROM `comment` AS c
-						INNER JOIN `article` AS a ON (c.article=a.id)
-						WHERE c.`active`=1
-						AND timestamp >= NOW() - INTERVAL 3 WEEK
-						AND a.published IS NOT NULL
-						AND a.published < NOW()
-						GROUP BY article
-						ORDER BY timestamp DESC
-						LIMIT 20)
-					UNION ALL
-						(SELECT ce.article,COUNT(*) AS count
-						FROM `comment_ext` AS ce
-						INNER JOIN `article` AS a ON (ce.article=a.id)
-						WHERE ce.`active` = 1
-						AND pending = 0
-						AND timestamp >= NOW() - INTERVAL 3 WEEK
-						AND a.published IS NOT NULL
-						AND a.published < NOW()
-						GROUP BY article
-						ORDER BY timestamp DESC)
+					SELECT c.article,COUNT(*) AS count
+					FROM `comment` AS c
+					INNER JOIN `article` AS a ON (c.article=a.id)
+					WHERE c.`active`=1
+					AND timestamp >= NOW() - INTERVAL 3 WEEK
+					AND a.published IS NOT NULL
+					AND a.published < NOW()
+					GROUP BY article
+					ORDER BY timestamp DESC
+					LIMIT 20
 				) AS t
 				GROUP BY article
 				ORDER BY count DESC, article DESC LIMIT %i",
