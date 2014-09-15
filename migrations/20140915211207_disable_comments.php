0cdf96b166691b6e8fa7f604e8c3d4d9d1cadfd4
Jonathan Kim <jkimbo@gmail.com>
2014-09-15 21:57:45 +0100

    Tabbing

--- migrations/20140915211207_disable_comments.php
+++ migrations/20140915211207_disable_comments.php
@@ -4,28 +4,28 @@ use Phinx\Migration\AbstractMigration;
 
 class DisableComments extends AbstractMigration
 {
-    /**
-     * Change Method.
-     *
-     * More information on this method is available here:
-     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
-     *
-     * Uncomment this method if you would like to use it.
+	/**
+	 * Change Method.
+	 *
+	 * More information on this method is available here:
+	 * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+	 *
+	 * Uncomment this method if you would like to use it.
 	 */
-    public function change()
-    {
+	public function change()
+	{
 		$table = $this->table('article');
-        $table->addColumn('comment_status', 'integer', array(
-            'null' => false,
+		$table->addColumn('comment_status', 'integer', array(
+			'null' => false,
 			'default' => 1,
 		))
-        ->save(); 
+		->save();
 
 		$this->execute('
 			UPDATE
 				article
 			SET comment_status = 0
 			WHERE published < date_sub(NOW(), interval 1 month)
-		'); 
-    }
+		');
+	}
 }
