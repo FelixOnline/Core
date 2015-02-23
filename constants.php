c8492ad0427f44116a68329deb358754ddbad59f
Philip Kent <philip.kent@me.com>
2015-02-23 13:08:08 +0000

    Fix typo in constants file

--- constants.php
+++ constants.php
@@ -9,7 +9,7 @@
 	if(!defined('SESSION_LENGTH'))					define('SESSION_LENGTH',7200); // session length
 	if(!defined('LOGIN_CHECK_LENGTH'))				define('LOGIN_CHECK_LENGTH',300); // length to allow login check (5mins)
 	if(!defined('COOKIE_LENGTH'))					define('COOKIE_LENGTH', 2592000); // cookie length (30 days) (60*60*24*30)
-	if(!defined('ARTICLES_PER_SEARCH_PAGE'))		define('ARTICLES_PER_CAT_PAGE',8); // number of articles on the first category page
+	if(!defined('ARTICLES_PER_SEARCH_PAGE'))		define('ARTICLES_PER_SEARCH_PAGE',8); // number of articles on the first category page
 	if(!defined('IMAGE_URL'))						define('IMAGE_URL', 'http://img.felixonline.co.uk/'); // image url
 	if(!defined('LOCAL'))							define('LOCAL', false); // if true then site is hosted locally - don't use pam_auth etc.
 
