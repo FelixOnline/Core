9fe64c9c37382f210e27c13a0c12f43e5b6de3c6
Philip Kent <philip.kent@me.com>
2015-02-23 12:20:32 +0000

    Implement Notices class for alerts boxes

---
+++ migrations/20150223121338_notices_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class NoticesTable extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     *
+     * Uncomment this method if you would like to use it.
+     */
+    public function change()
+    {
+        $table = $this->table('notices');
+        $table->addColumn('author', 'string', array('limit' => 16))
+              ->addColumn('text', 'integer')
+              ->addColumn('start_time', 'datetime')
+              ->addColumn('end_time', 'datetime')
+              ->addColumn('hidden', 'boolean')
+              ->addColumn('frontpage', 'boolean')
+              ->addColumn('sort_order', 'integer')
+              ->create();
+
+        $table->addForeignKey('text', 'text_story', 'id', array('delete'=> 'CASCADE', 'update'=> 'CASCADE'))
+              ->addForeignKey('author', 'user', 'user', array('delete'=> 'CASCADE', 'update'=> 'CASCADE'))
+              ->save();
+
+    }
+}
---
+++ src/FelixOnline/Core/Notice.php
@@ -0,0 +1,55 @@
+<?php
+namespace FelixOnline\Core;
+/*
+ * Notice class
+ */
+class Notice extends BaseDB
+{
+	public $dbtable = 'notice';
+
+	function __construct($id = NULL) {
+		$fields = array(
+			'author' => new ForeignKey('User'),
+			'text' => new Type\ForeignKey('FelixOnline\Core\Text'),
+			'start_date' => new Type\DateTimeField(),
+			'end_date' => new Type\DateTimeField(),
+			'hidden' => new Type\BooleanField(array(
+				'null' => false,
+			)),
+			'frontpage' => new Type\BooleanField(array(
+				'null' => false,
+			)),
+			'sort_order' => new Type\IntegerField(),
+		);
+
+		parent::__construct($fields, $id);
+	}
+
+	public function isEnded() {
+		if($this->getHidden() == TRUE || $this->getStartDate() < NOW() || $this->getEndDate() > NOW()) {
+			return true;
+		}
+
+		return false;
+	}
+
+	/**
+	 * Private: Clean text
+	 */
+	private function cleanText($text) {
+		$result = strip_tags($text, '<b><i><u><img><a><strong><em>'); // Gets rid of html tags except for a few
+		$result = preg_replace('/(<[^>]+) style=".*?"/i', '$1', $result); // Remove style attributes
+		return $result;
+	}
+
+	/**
+	 * Public: Get article content
+	 */
+	public function getContent() {
+		$string = $this->getText()->getContent();
+
+		$string = $this->cleanText($string);
+
+		return $string;
+	}
+}
