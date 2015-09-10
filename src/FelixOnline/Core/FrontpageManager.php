217ed0673f7e029e9d963f733968ac2fcf416423
Philip Kent <philip.kent@me.com>
2015-09-10 18:57:53 +0100

    Handle frontpage article category secrecy in provided manager

--- src/FelixOnline/Core/FrontpageManager.php
+++ src/FelixOnline/Core/FrontpageManager.php
@@ -11,8 +11,46 @@ class FrontpageManager extends BaseManager
 	function getSection($section) {
 		$articles = $this
 				->filter('section = "%s"', array($section))
-				->order('sort_order', 'ASC')
-				->values();
+				->order('sort_order', 'ASC');
+
+		$articleManager = BaseManager::build('FelixOnline\Core\Article', 'article');
+
+		$categoryManager = BaseManager::build('FelixOnline\Core\Category', 'category');
+
+		global $currentuser;
+
+		if(!$currentuser->isLoggedIn()) {
+			$categoryManager->filter('secret = 0');
+		}
+
+		$articleManager->join($categoryManager, null, 'category');
+
+		$articles->join($articleManager, null, 'article');
+
+		$articles = $articles->values();
+
+		return $articles;
+	}
+
+	function getAll() {
+		$articles = $this
+				->order('sort_order', 'ASC');
+
+		$articleManager = BaseManager::build('FelixOnline\Core\Article', 'article');
+
+		$categoryManager = BaseManager::build('FelixOnline\Core\Category', 'category');
+
+		global $currentuser;
+
+		if(!$currentuser->isLoggedIn()) {
+			$categoryManager->filter('secret = 0');
+		}
+
+		$articleManager->join($categoryManager, null, 'category');
+
+		$articles->join($articleManager, null, 'article');
+
+		$articles = $articles->values();
 
 		return $articles;
 	}
